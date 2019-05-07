986d8c7747ee4f3efa00b2e9ccd5947c38da10e0
trink <treelink@126.com>
2019-05-07 23:33:45 +0800

    swagger

--- web/swagger.php
+++ web/swagger.php
@@ -1,13 +1,13 @@
 <?php
 
-$openapiPath = __DIR__ . '/openapi.yaml';
-if (file_exists($openapiPath)) {
-    unlink($openapiPath);
+$openApiPath = __DIR__ . '/openapi.yaml';
+if (file_exists($openApiPath)) {
+    unlink($openApiPath);
 }
-$controllersDir = "D:\Dev\Projects\www\study-yii\controllers";
-$openapiDir     = "D:\Dev\Projects\www\swagger-php\bin\openapi";
+$controllersDir = dirname(__DIR__) . "/controllers";
+$openApiDir     = dirname(dirname(__DIR__)) . "/swagger-php/bin/openapi";
 $targetApiDir   = realpath(__DIR__);
-$message        = exec("php71 \"{$openapiDir}\" \"{$controllersDir}\" -o \"{$targetApiDir}\"");
+$message        = exec("php71 \"{$openApiDir}\" \"{$controllersDir}\" -o \"{$targetApiDir}\"");
 if (! empty($message)) {
     $happenTime = str_repeat("-", 10) . date('Y-m-d H:i:s') . str_repeat("-", 10);
     file_put_contents('logs/' . date('Y_m_d') . '.log', "{$happenTime}\n{$message}\n\n", FILE_APPEND);
