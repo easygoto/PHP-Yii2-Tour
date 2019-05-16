2ecb825b9357408789922556d3c2dda6047f5be0
trink <treelink@126.com>
2019-05-16 21:57:59 +0800

    swagger

--- web/swagger.php
+++ web/swagger.php
@@ -7,7 +7,7 @@ if (file_exists($openApiPath)) {
 $controllersDir = dirname(__DIR__) . "/controllers";
 $openApiDir     = dirname(dirname(__DIR__)) . "/swagger-php/bin/openapi";
 $targetApiDir   = realpath(__DIR__);
-$message        = exec("php71 \"{$openApiDir}\" \"{$controllersDir}\" -o \"{$targetApiDir}\"");
+$message        = exec("php7 \"{$openApiDir}\" \"{$controllersDir}\" -o \"{$targetApiDir}\"");
 if (! empty($message)) {
     $happenTime = str_repeat("-", 10) . date('Y-m-d H:i:s') . str_repeat("-", 10);
     file_put_contents('logs/' . date('Y_m_d') . '.log', "{$happenTime}\n{$message}\n\n", FILE_APPEND);
